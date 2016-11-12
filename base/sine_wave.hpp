165e49903e1cd5f48a298de7013b7a14bd3d2274
Darren Otgaar <darren.otgaar@icloud.com>
2016-11-12 11:25:00 +0200

    - Square & sine wave, simple mixer

--- base/sine_wave.hpp
+++ base/sine_wave.hpp
@@ -5,12 +5,32 @@
 #ifndef ZAPAUDIO_SINE_WAVE_HPP
 #define ZAPAUDIO_SINE_WAVE_HPP
 
+#include <cmath>
 #include <cassert>
 #include "audio_stream.hpp"
+
+constexpr double PI = 3.14159265358979323846;
+constexpr double TWO_PI = 2.0 * PI;
 
 /*
  * An audio_stream implementation of a 440 Hz sine wave.
  */
+
+// A saturated add
+
+short sadd(const short& lhs, const short& rhs) {
+    if(lhs >= 0) {
+        if(std::numeric_limits<short>::max() - lhs < rhs) {
+            return std::numeric_limits<short>::max();
+        }
+    } else {
+        if(rhs < std::numeric_limits<short>::lowest() - lhs) {
+            return std::numeric_limits<short>::lowest();
+        }
+    }
+
+    return lhs + rhs;
+}
 
 template <typename SampleT>
 class sine_wave : public audio_stream<SampleT> {
@@ -26,11 +46,15 @@ public:
     virtual size_t read(typename audio_stream<SampleT>::buffer_t& buffer, size_t len) {
         for(size_t i = 0, end = len/channels_; i != end; ++i) {
 
-            // Sawtooth wave for now
-            short value = curr_sample_ < (period_ / 2) ? std::numeric_limits<short>::max() : std::numeric_limits<short>::lowest();
+            // Square wave for now
+            short value1 = curr_sample_ < (period_ / 2) ? std::numeric_limits<short>::max() : std::numeric_limits<short>::lowest();
+
+            // Plus a sine wave
+            float x = curr_sample_ / float(period_);
+            short value2 = short(std::numeric_limits<short>::max()*std::sin(TWO_PI * x));
 
             for(int ch = 0; ch != channels_; ++ch) {
-                buffer[channels_*i + ch] = value;
+                buffer[channels_*i + ch] = sadd(value1/60, value2);
             }
 
             curr_sample_ = (curr_sample_ + 1) % period_;
