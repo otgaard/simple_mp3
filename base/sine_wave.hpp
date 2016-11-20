3174743f795138edc6d64fba70aa8a1bb68343de
Darren Otgaar <darren.otgaar@icloud.com>
2016-11-20 12:30:39 +0200

    - Sine wave stream for tuning

--- base/sine_wave.hpp
+++ base/sine_wave.hpp
@@ -9,28 +9,9 @@
 #include <cassert>
 #include "audio_stream.hpp"
 
-constexpr double PI = 3.14159265358979323846;
-constexpr double TWO_PI = 2.0 * PI;
-
 /*
  * An audio_stream implementation of a 440 Hz sine wave.
  */
-
-// A saturated add
-
-short sadd(const short& lhs, const short& rhs) {
-    if(lhs >= 0) {
-        if(std::numeric_limits<short>::max() - lhs < rhs) {
-            return std::numeric_limits<short>::max();
-        }
-    } else {
-        if(rhs < std::numeric_limits<short>::lowest() - lhs) {
-            return std::numeric_limits<short>::lowest();
-        }
-    }
-
-    return lhs + rhs;
-}
 
 template <typename SampleT>
 class sine_wave : public audio_stream<SampleT> {
@@ -41,19 +22,23 @@ public:
     }
     virtual ~sine_wave() = default;
 
+    void set_hertz(size_t hertz) {
+        hertz_ = hertz;
+        period_ = std::max(sample_rate_ / hertz, size_t(1));
+    }
+
+    size_t get_hertz() const { return hertz_; }
+
     virtual size_t read(typename audio_stream<SampleT>::buffer_t& buffer, size_t len) {
+        constexpr double PI = 3.14159265358979323846;
+        constexpr double TWO_PI = 2.0 * PI;
+
         for(size_t i = 0, end = len/channels_; i != end; ++i) {
 
-            // Square wave for now
-            short value1 = curr_sample_ < (period_ / 2) ? std::numeric_limits<short>::max() : std::numeric_limits<short>::lowest();
-
-            // Plus a sine wave
             float x = curr_sample_ / float(period_);
-            short value2 = short(std::numeric_limits<short>::max()*std::sin(TWO_PI * x));
+            short value = short(std::round(std::numeric_limits<short>::max()*std::sin(TWO_PI * x)));
 
-            for(int ch = 0; ch != channels_; ++ch) {
-                buffer[channels_*i + ch] = sadd(value1/60, value2);
-            }
+            for(int ch = 0; ch != channels_; ++ch) buffer[channels_*i + ch] = value;
 
             curr_sample_ = (curr_sample_ + 1) % period_;
         }
